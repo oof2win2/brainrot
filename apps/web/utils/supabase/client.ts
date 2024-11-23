e2e2a59974fc18923857a669eba74d056d0ecc66
oof2win2 <honza.koco44@gmail.com>
2024-11-22 21:15:02 -0800

    add supabase clients

---
+++ apps/web/utils/supabase/client.ts
@@ -0,0 +1,11 @@
+"use client";
+
+import { createBrowserClient } from "@supabase/ssr";
+import { env } from "@/env";
+
+export function createClient() {
+  return createBrowserClient(
+    env.NEXT_PUBLIC_SUPABASE_PROJECT_URL,
+    env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  );
+}
